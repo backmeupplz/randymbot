5d7e3e48ffe97ee9a7b65632044ab068379438ca
backmeupplz <backmeupplz@gmail.com>
2019-09-05 08:31:07 -0700

    add /number format

--- src/commands/number.ts
+++ src/commands/number.ts
@@ -9,15 +9,28 @@ import { loc } from '../helpers/locale'
  * @param bot Bot to setup the command
  */
 export function setupNumber(bot: Telegraf<ContextMessageUpdate>) {
-  bot.command('number', async (ctx) => {
+  bot.command('number', async ctx => {
     // Check if admin
     const isAdmin = await checkIfAdmin(ctx)
     if (!isAdmin) return
     // Get chat
-    const chat = await findChat(ctx.chat.id)
+    let chat = await findChat(ctx.chat.id)
+    // Check if `/number XXX` format
+    const numberString = ctx.message.text.substr(8).trim()
+    if (
+      numberString &&
+      !isNaN(+numberString) &&
+      +numberString > 0 &&
+      +numberString < 10000000
+    ) {
+      chat.number + numberString
+      chat = await chat.save()
+      // Reply
+      return ctx.reply(loc('number_selected', chat.language))
+    }
     // Reply
-    ctx.reply(loc('select_number', chat.language), {
-      reply_markup: getButtons()
+    return ctx.reply(loc('select_number', chat.language), {
+      reply_markup: getButtons(),
     })
   })
 }
@@ -27,10 +40,10 @@ export function setupNumber(bot: Telegraf<ContextMessageUpdate>) {
  * @param bot Bot to setup the callback
  */
 export function setupNumberCallback(bot: Telegraf<ContextMessageUpdate>) {
-  (<any>bot).action(async (data: string, ctx: ContextMessageUpdate) => {
+  ;(<any>bot).action(async (data: string, ctx: ContextMessageUpdate) => {
     // Get raffle
     const datas = data.split('~')
-    if (datas[0] !== 'n') return;
+    if (datas[0] !== 'n') return
     // Check if admin
     const isAdmin = await checkIfAdmin(ctx, false)
     if (!isAdmin) return
@@ -40,7 +53,12 @@ export function setupNumberCallback(bot: Telegraf<ContextMessageUpdate>) {
     chat.number = Number(datas[1])
     chat = await chat.save()
     // Update message
-    await ctx.telegram.editMessageText(ctx.chat.id, (<any>ctx).update.callback_query.message.message_id, undefined, loc('number_selected', chat.language))
+    await ctx.telegram.editMessageText(
+      ctx.chat.id,
+      (<any>ctx).update.callback_query.message.message_id,
+      undefined,
+      loc('number_selected', chat.language)
+    )
   })
 }
 
@@ -51,18 +69,24 @@ export function setupNumberCallback(bot: Telegraf<ContextMessageUpdate>) {
 function getButtons() {
   return {
     inline_keyboard: [
-      [{ text: '1', callback_data: `n~1` },
-      { text: '2', callback_data: `n~2` },
-      { text: '3', callback_data: `n~3` },
-      { text: '4', callback_data: `n~4` }],
-      [{ text: '5', callback_data: `n~5` },
-      { text: '6', callback_data: `n~6` },
-      { text: '7', callback_data: `n~7` },
-      { text: '8', callback_data: `n~8` }],
-      [{ text: '9', callback_data: `n~9` },
-      { text: '10', callback_data: `n~10` },
-      { text: '11', callback_data: `n~11` },
-      { text: '12', callback_data: `n~12` }],
+      [
+        { text: '1', callback_data: `n~1` },
+        { text: '2', callback_data: `n~2` },
+        { text: '3', callback_data: `n~3` },
+        { text: '4', callback_data: `n~4` },
+      ],
+      [
+        { text: '5', callback_data: `n~5` },
+        { text: '6', callback_data: `n~6` },
+        { text: '7', callback_data: `n~7` },
+        { text: '8', callback_data: `n~8` },
+      ],
+      [
+        { text: '9', callback_data: `n~9` },
+        { text: '10', callback_data: `n~10` },
+        { text: '11', callback_data: `n~11` },
+        { text: '12', callback_data: `n~12` },
+      ],
     ],
   }
 }
