22ca0393b4d41b1da90f946511489109eb0faf66
backmeupplz <backmeupplz@gmail.com>
2021-11-05 13:37:19 -0700

    fix

--- src/commands/debug.ts
+++ src/commands/debug.ts
@@ -2,7 +2,7 @@ import { Telegraf, ContextMessageUpdate } from 'telegraf'
 
 export function setupDebug(bot: Telegraf<ContextMessageUpdate>) {
   bot.command('debug', async (ctx) => {
-    if (ctx.from?.id !== +process.env.ADMIN) {
+    if (ctx.from && ctx.from.id !== parseInt(process.env.ADMIN, 10)) {
       return
     }
     return ctx.replyWithHTML(
